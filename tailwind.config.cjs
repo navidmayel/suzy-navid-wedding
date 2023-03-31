4c86ed7b20d415f62ebe7438ba8de73bd242d071
navidmayel <navidmayel@gmail.com>
2023-03-31 10:42:51 +0400

    font typo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        "dark-terra": "#D46A53",
-        "light-terra": "#F29979",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
